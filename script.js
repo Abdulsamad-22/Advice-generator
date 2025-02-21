ea23051dfdfb55bddbfd6f3c05e1f82f801f7f11
unknown <abdulsamadhussaini001@gmail.com>
2025-02-21 19:33:24 +0100

    refactored code base

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-/*
-const xhr = new XMLHttpRequest();
-
-xhr.addEventListener('load', () => {
-    console.log(xhr.response);
-});
-
-xhr.open('GET', 'https://api.adviceslip.com/advice');
-
-xhr.send();
-
-*/
-
 const adviceText = document.querySelector('.advice');
 const adviceId = document.querySelector('.advice-id');
 const button = document.querySelector('.play');
 
 
-function fetchAdvice() {
+async function fetchAdvice() {
+    try {
+        // Fetch advice data from API
+       const response = await fetch('https://api.adviceslip.com/advice?t=${new Date().getTime()}');
 
+       const data = await response.json();
+
+       // Display advice and corresponding id in DOM
+       const {advice, id} = data.slip;
+        adviceId.textContent = `ADVICE #${id}`;
+        adviceText.textContent = `"${advice}"`;
+
+    } catch (error) {
+        adviceText.textContent = 'Failed to fetch advice, Please try again.';
+        console.error('Error fetching advice:', error.message);
+    }
+    /*
+
+    // Practicing promises
     fetch('https://api.adviceslip.com/advice').then( (response) => {
         const reponse = response.json();
         return reponse
@@ -27,44 +31,9 @@ function fetchAdvice() {
         adviceId.textContent = `ADVICE #${id}`;
         adviceText.textContent = `"${advice}"`;
     });
+    */
 }
-
-button.addEventListener('click', fetchAdvice);
-
-fetchAdvice();
-
-/*
-async function adviceFetch() {
-    try {
-        const response = await fetch('https://api.adviceslip.com/advice', {
-            cache: 'no-cache'
-        });
-
-        // Await the JSON response
-        const data = await response.json();
-
-        const advice = data.slip.advice;
-        const adviceId = data.slip.id;
-
-        document.querySelector('.id').textContent = `${adviceId}`;
-
-        document.querySelector('.advice').textContent = `"${advice}"`;
-
-        console.log(advice);
-        console.log(`Advice ID: ${adviceId}`);
-    } catch (error) {
-        console.error('Error fetching advice:', error.message);
-    }
-}
-
-
-
-console.log(document.querySelector('.play'));
-
-// Ensure script runs after DOM loads
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelector('.play').addEventListener('click', adviceFetch);
-    adviceFetch(); // Fetch advice on page load
+    
+button.addEventListener('click', () => {
+    fetchAdvice();
 });
-
-*/
